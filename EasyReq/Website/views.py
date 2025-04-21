388bb4673722d937029f5442f6152b04da0fca6a
eden1572 <160607307+eden1572@users.noreply.github.com>
2025-04-21 15:00:23 +0300

    Update views.py
    login page commit

--- EasyReq/Website/views.py
+++ EasyReq/Website/views.py
@@ -43,15 +43,25 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect("/Website/home")
     return render(request, 'login.html')
-    
+
 def login_request(request):
     if request.user.is_authenticated:
         return redirect('home')
+
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect("/Website/home")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'login.html', {'form': AuthenticationForm()})
             
 @login_required
 def logout_view(request):

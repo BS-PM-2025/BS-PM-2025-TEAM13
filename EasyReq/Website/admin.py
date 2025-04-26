720a84a252935dbac2787bc7972d66f461ff0d06
aviramorad <aviramo@ac.sce.ac.il>
2025-04-26 21:17:57 +0300

    some fixes and improvements

--- EasyReq/Website/admin.py
+++ EasyReq/Website/admin.py
@@ -8,15 +8,20 @@ class DepartmentAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'dept')
-    search_fields = ('name', 'dept__name')
+    list_display = ('id', 'name', 'dept', 'year')
+    search_fields = ('name', 'dept__name', 'year')
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'email', 'role', 'department')
-    list_filter = ('role', 'department')
+    list_display = ('username', 'first_name', 'email', 'role', 'department', 'is_active')
+    list_filter = ('role', 'department', 'is_active')
     search_fields = ('username', 'name', 'email', 'department__name')
     ordering = ('username',)
+    actions = ['approve_selected_users']
 
+    def approve_selected_users(self, request, queryset):
+        queryset.update(is_active=True)
+
+    approve_selected_users.short_description = "Approve selected users"
 class RequestAdmin(admin.ModelAdmin):
     list_display = ('title', 'student', 'dept', 'status', 'created')
     list_filter = ('status', 'student')
--- EasyReq/Website/views.py
+++ EasyReq/Website/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.generic import CreateView
-from .models import User
+from .models import User, Course
 from .forms import RegistrationForm
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -20,18 +20,32 @@ def register_view(request):
         form = RegistrationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save(commit=False)
-            if not user.profile_pic:
-                user.profile_pic = 'images/profile.png'
             user.save()
-            return redirect("/Website/login")
+           # if user.role == 1:
+           #     user.is_active = 0
+            if user.role == 0:
+                relevant_courses = Course.objects.filter(year=user.year, dept=user.department)
+                if relevant_courses.exists():
+                    user.courses.add(*relevant_courses)
+            return redirect('registration_success', user_id=user.id)
     else:
         form = RegistrationForm()
     return render(request, 'register.html', {"form": form})
+
+def registration_success(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        role = user.role
+        context = {'user': user, 'role': role}
+        return render(request, 'registration_success.html', context)
+    except User.DoesNotExist:
+        return redirect('register')
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("/Website/home")
     return render(request, 'login.html')
+
 
 def login_request(request):
     if request.user.is_authenticated:
@@ -39,23 +53,33 @@ def login_request(request):
 
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
+        username = form.data.get('username')
+        try:
+            user_obj = User.objects.get(username=username)
+            if not user_obj.is_active:
+                messages.error(request, "המשתמש שלך ממתין לאישור מנהל מערכת")
+                return HttpResponseRedirect("/login")
+        except User.DoesNotExist:
+            pass
+
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return HttpResponseRedirect("/Website/home")
+                return HttpResponseRedirect("")
             else:
-                messages.error(request, "Invalid username or password")
+                messages.error(request, "שגיאה באחד מפרטי הזיהוי")
         else:
-            messages.error(request, "Invalid username or password")
+            messages.error(request, "שגיאה באחד מפרטי הזיהוי")
+
     return render(request, 'login.html', {'form': AuthenticationForm()})
 
 @login_required
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect("/Website/login")
+    return redirect("home")
 
 class Registration(CreateView):
     model = User
@@ -64,26 +88,25 @@ class Registration(CreateView):
 
     def form_valid(self, form):
         user = form.save()
-        login(self.request, user)
-        return redirect('/Website/home/')
+        path_ = "/registration-success/" + str(user.id)
+        return redirect(path_, user_id=user.id)
 
 
 @login_required
 def profile_view(request):
     user = request.user
-
     if request.method == 'POST':
         if 'profile_pic' in request.FILES:
             user.profile_pic = request.FILES['profile_pic']
             user.save()
-            messages.success(request, "Profile picture updated successfully!")
+            messages.success(request, "!תמונת הפרופיל עודכנה בהצלחה")
 
         if 'old_password' in request.POST:
             password_form = PasswordChangeForm(user, request.POST)
             if password_form.is_valid():
                 user = password_form.save()
                 update_session_auth_hash(request, user)
-                messages.success(request, "Password updated successfully!")
+                messages.success(request, "!הסיסמה עודכנה בהצלחה")
             else:
                 for error in password_form.errors.values():
                     messages.error(request, error)

1281837e75f98c779d8d27b82b74471a5ec37aeb
eden1572 <160607307+eden1572@users.noreply.github.com>
2025-05-13 11:17:27 +0300

    Update urls.py
    BSPM25T13-127 & BSPM25T13-131 & BSPM25T13-135 - export's urls commit

--- EasyReq/Website/urls.py
+++ EasyReq/Website/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import export_requests_excel
 
 urlpatterns = [
     path('register/', views.Registration.as_view(), name = 'register'),
@@ -18,6 +19,12 @@ urlpatterns = [
          (template_name="password_reset_done.html"), name= "password_reset_complete"),
     path('profile/', views.profile_view, name='profile'),
 
+    path('requests/export/csv/', views.export_requests_csv, name='export_requests_csv'),
+    path('requests/export/excel/', views.export_requests_excel, name='export_requests_excel'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/export/excel/', views.export_dashboard_excel, name='export_dashboard_excel'),
+    
     path('mark_notification_read/', views.mark_notification_read, name='mark_notification_read'),
     path('mark_all_notifications_read/', views.mark_all_notifications_read, name='mark_all_notifications_read'),
     path('all_notifications/', views.all_notifications, name='all_notifications'),

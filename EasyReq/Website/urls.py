e034127e0e74e3f6d07ab0ebfbcc6ce7a9d1518e
eden1572 <160607307+eden1572@users.noreply.github.com>
2025-05-13 10:01:59 +0300

    Update urls.py
    BSPM25T13-110 & BSPM25T13-113 & BSPM25T13-116 & BSPM25T13-117 - notification's urls

--- EasyReq/Website/urls.py
+++ EasyReq/Website/urls.py
@@ -17,4 +17,9 @@ urlpatterns = [
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view
          (template_name="password_reset_done.html"), name= "password_reset_complete"),
     path('profile/', views.profile_view, name='profile'),
+
+    path('mark_notification_read/', views.mark_notification_read, name='mark_notification_read'),
+    path('mark_all_notifications_read/', views.mark_all_notifications_read, name='mark_all_notifications_read'),
+    path('all_notifications/', views.all_notifications, name='all_notifications'),
+    path('toggle_notification_status/', views.toggle_notification_status, name='toggle_notification_status'),
 ]
